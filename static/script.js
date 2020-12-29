a679117daa014e30f8f3f27dac13803a457bca87
Claire Casey <clcasey805@gmail.com>
2020-12-29 13:21:51 -0800

    update styling and add legend

--- static/script.js
+++ static/script.js
@@ -1,9 +1,5 @@
 "use strict";
 
-// const BASE_URL = "https://health.data.ny.gov/resource/xdss-u53e.json";
-
-// const $locations = $(".location");
-
 /* Function makes a get request to API */
 
 async function countyData(county_name) {
@@ -17,44 +13,33 @@ async function countyData(county_name) {
   return response.data
 }
 
-/* Handles a form submission. */
+/* Creates a chart given data on a county. */
 
 function createChart(data) {
-  // const $chart = $('<div class="bar-chart">');
-  // const $svg = $(``);
-  // const $container = $('#container');
-  // console.log($svg);
+  // remove spaces and periods from county name
   const county = data[0].county.split(' ').join('').split('.').join('');
-  // $svg
-  //   .attr("height", "600")
-  //   .attr("width", "1000")
-  //   .attr('id', county)
-  //   .addClass("p-10");
-  // $chart.append($svg);
-  // $container.append($chart);
 
 
   const margin = 60;
   const width = 1000 - 2 * margin;
   const height = 600 - 2 * margin;
   const svg = d3.select(`#${county}`);
-  console.log(svg);
+
   const chart = svg.append('g')
     .attr('transform', `translate(${margin}, ${margin})`);
-  console.log('data is', data);
+
+  // get max positive to set height of chart
   const newPostitives = data.map(s => s.new_positives);
   const maxPos = Math.max(...newPostitives);
-  console.log('max is', maxPos);
+  // set scales and append
   const yScale = d3.scaleLinear()
     .range([height, 0])
     .domain([0, maxPos]);
   chart.append('g')
     .call(d3.axisLeft(yScale));
-
   const xScale = d3.scaleTime()
     .range([0, width])
     .domain(d3.extent(data.map((s) => Date.parse(s.test_date))))
-
   chart.append('g')
     .attr('transform', `translate(0, ${height})`)
     .call(d3.axisBottom(xScale)
@@ -77,8 +62,6 @@ function createChart(data) {
     .on('mouseover', function (evt, d) {
       const date = new Date(d.test_date);
       d3.select(this)
-      // .transition()
-      // .duration(200)
         .style('opacity', 1)
       d3.select('#cases').text(`Daily New Cases: ${d.new_positives}`);
       d3.select('#date').text(`Date: ${date.toDateString()}`);
@@ -86,39 +69,17 @@ function createChart(data) {
         .style('opacity', 0.8)
         .style("display", "block")
         .style("left", (evt.pageX - 10) + "px")
-        .style("top", (evt.pageY - 100) + "px");    
-      })
-    .on('mouseout', function() {
+        .style("top", (evt.pageY - 100) + "px");
+    })
+    .on('mouseout', function () {
       d3.select(this)
-      .style('opacity', 0.5)
+        .style('opacity', 0.5)
     })
 
-  // chart.selectAll("bar")
-  //   .data(data)
-  //   .enter().append("rect")
-  //   .style("fill", "blue")
-  //   .style("opacity", 0.5)
-  //   .style("pointer-events","all")
-  //   .attr("x", (s) => xScale(Date.parse(s.test_date)))
-  //   .attr("y", (s) => yScale(s.new_positives))
-  //   .attr('height', (s) => height - yScale(s.new_positives))
-  //   .attr('width', width / data.length)
-  //   .on('mouseenter', function (actual, i) {
-  //     console.log('MOUSE', this)
-  //     d3.select(this)
-  //     .transition()
-  //     .duration(200)
-  //       .style('opacity', 1)
-  //     d3.select('#cases').text(`${d.new_positives}`)
-
-    // });
-
-
   addLabels(svg, height, margin, width, data)
-
-
-  // $("body").html($("body").html());
 }
+
+/* Add labels ot chart */
 
 function addLabels(svg, height, margin, width, data) {
   svg.append('text')
@@ -130,31 +91,25 @@ function addLabels(svg, height, margin, width, data) {
 
   svg.append('text')
     .attr('x', width / 2 + margin)
-    .attr('y', (margin))
+    .attr('y', (margin + 10))
     .attr('text-anchor', 'middle')
     .text(`${data[0].county}`)
 }
 
 
 
-
-
-
-
-// let data = [];
-
-/* On starting page, add event listener on like buttons */
+/* On starting page, create charts for each location a 
+user is following */
 
 async function start() {
   const $locations = $(".location");
-  console.log('LOCATIONS ARE', $locations);
-  console.log($locations);
+  // console.log('LOCATIONS ARE', $locations);
+  // console.log($locations);
   for (let loc of $locations) {
     let data = [];
-    // let loc = $locations[0]
     let $loc = $(loc);
-    let location = $loc.text()
-    console.log('location', location)
+    let location = $loc.text().trim();
+    console.log('location', location);
     data = await countyData(location);
     createChart(data);
   }
